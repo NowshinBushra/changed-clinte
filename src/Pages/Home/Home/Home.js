fe0e1ab3f5fa96d8df721c982bd7fe13338f8812
NowshinBushra <nowshin.bushra399@gmail.com>
2023-06-12 17:46:27 +0600

    second commit

--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import Banner from '../Banner/Banner';
 import Gallery from '../Gallery/Gallery';
+import ByAge from '../Shop/ByAge';
+import ByBrand from '../Shop/ByBrand';
 
 const Home = () => {
     return (
         <div>
             <Banner></Banner>
             <Gallery></Gallery>
+            <h2 className='text-4xl font-bold text-accent text-center mt-16'>Shop by category</h2>
+            <ByAge></ByAge>
+            <ByBrand></ByBrand>
         </div>
     );
 };
